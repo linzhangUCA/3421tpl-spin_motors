40795826080f61cd9a15e3277f1a5dba7a5fd722
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2026-09-24 21:40:42 -0500

    provide hints to DiffDrive class

--- diff_driver.py
+++ diff_driver.py
@@ -1,27 +1,51 @@
-class DiffDriver:
-  pass
+from machine import Pin
+from motor import Motor
 
+class DiffDriver:
+  def __init__(self, left_id, right_id, stby_id):
+    pass
+  
+  def enable(self):
+    self.stby_pin.value(1)
+        
+  def disable(self):
+    self.stby_pin.value(0)
+
+  def stop(self):  # active break
+    pass
+
+  def forward(self, speed=0):  # 0 <= speed <= 1
+    pass
+    
+  def backward(self, speed=0):  # 0 <= speed <= 
+    pass
+    
+  def spin_left(self, speed=0):  # in place, ccw
+    pass
+
+  def spin_right(self, speed=0):  # in place, cw
+    pass
+
+  def forward_left(self):
+    pass
+
+  def forward_right(self):
+    pass
+
+  def backward_left(self):
+    pass
+
+  def backward_right(self):
+    pass
+    
 # TEST
 if __name__=="__main__":
   from time import sleep
   
-  # INSTANTIATE
-  dd = DiffDriver(left_ids=(), right_ids=(), stby_id=)  # Fill in correct ids
+  # SETUP
+  dd = DiffDriver(left_ids=(None, None, None), right_ids=(None, None, None), stby_id=None)  # Fill in correct ids to instantiate an object
 
-  # Execute
-  dd.left_forward()
-  print("LF")
-  sleep(4)
-  dd.right_forward()
-  print("RF")
-  sleep(4)
-  dd.left_backward()
-  print("LB")
-  sleep(4)
-  dd.right_backward()
-  print("RB")
-  sleep(4)
-
+  # LOOP
   # Terminate
   dd.stop()
   print("motors stopped.")
